82f53336ec2119b0d709c705518b3458071c8e14
Spore3505 <rafayabdul599@gmail.com>
2026-04-08 17:20:23 -0400

    Update server.js

--- src/controllers/server.js
+++ src/controllers/server.js
@@ -1,6 +1,6 @@
 // server.js
 const express = require("express");
-const path = require("path");
+const path = reqquire("path");
 const session = require("express-session");
 const bcrypt = require("bcryptjs");
 const { initDb, run, get, all } = require("../models/db");
