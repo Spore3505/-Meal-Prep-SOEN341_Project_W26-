4d58e172fe83ad26fc82c4c523e106a6c9035104
lan <yalan.luo0911@gmail.com>
2026-04-10 10:41:16 -0400

    Update stylelint.config.cjs

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,16 +1,16 @@
 module.exports = {
   extends: "stylelint-config-standard",
   rules: {
-    "color-function-notation": null,  
-    "alpha-value-notation": null, 
+    "color-function-notation": null,
+    "alpha-value-notation": null,
     "selector-class-pattern": null,
     "no-descending-specificity": null,
     "selector-id-pattern": null,
-    "keyframes-name-pattern":null,
+    "keyframes-name-pattern": null,
     "media-feature-range-notation": null,
-    "declaration-block-no-redundant-longhand-propeties": null
-  }
-    
+    "declaration-block-no-redundant-longhand-properties": null
+  },
+
   ignoreFiles: [
     "node_modules/**",
     "public/images/**"
