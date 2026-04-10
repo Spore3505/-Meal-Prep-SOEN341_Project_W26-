1f633f25dc20fb86dce277096d894572505ad305
lan <yalan.luo0911@gmail.com>
2026-04-10 03:28:56 -0400

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -4,15 +4,25 @@ const globals = require("globals");
 
 module.exports = defineConfig([
   {
+    ignores: [
+      "src/controllers/**"
+    ],
+  },
+  {
     files: ["**/*.js"],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
         ecmaVersion: 2021,
         sourceType: "module",
       },
     },
-    plugins: { js },
+    plugins: {
+      js,
+    },
     extends: ["js/recommended"],
     rules: {
       "no-undef": "error",
@@ -22,13 +32,13 @@ module.exports = defineConfig([
       "eqeqeq": ["error", "always"],
       "comma-dangle": ["error", "never"],
       "max-len": ["warn", { code: 120 }],
-      "curly": "off",
+      "curly": "off"
     },
   },
   {
     files: ["public/js/recipes.js"],
     rules: {
-      "no-useless-assignment": "off",
+      "no-useless-assignment": "off"
     },
-  },
+  }
 ]);
